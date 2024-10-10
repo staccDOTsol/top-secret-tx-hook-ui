29b748c85a95c1a8a2056c4269d10f718744b535
stacc <jarettrsdunn+git@gmail.com>
2024-10-10 17:16:11 +0100

    pondeing

--- pages/api/arbitrage.ts
+++ pages/api/arbitrage.ts
@@ -31,7 +31,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const walletPublicKey = new PublicKey(publicKeyBase58);
         const startingAmount = Number(amount);
   
-        const poolsResponse = await fetch('http://localhost:3000/api/whirlpools');
+        const poolsResponse = await fetch('https://regardio.fomo3d.fun/api/whirlpools');
         const poolsData = await poolsResponse.json();
   
         const graph = buildTokenGraphFromPools(poolsData.pools || poolsData);
