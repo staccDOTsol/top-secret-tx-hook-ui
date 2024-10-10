81753e289b8b98a9e5fc3c5821a44d13b8942799
stacc <jarettrsdunn+git@gmail.com>
2024-10-10 20:28:15 +0100

    pondeing

--- components/orca-whirlpools-with-transfer-hook.tsx
+++ components/orca-whirlpools-with-transfer-hook.tsx
@@ -69,7 +69,7 @@ export function OrcaWhirlpoolsWithTransferHook() {
 
        const signed=   await wallet.signAllTransactions([tx, transaction])
         const sig1 = await connection.sendRawTransaction(signed[0].serialize());
-        await new Promise(resolve => setTimeout(resolve, 10000)); // Wait for 10 seconds
+        await new Promise(resolve => setTimeout(resolve, 15000)); // Wait for 10 seconds
         // Simulate the transaction
         const simulationResult = await connection.simulateTransaction(signed[1]);
         
